bba315830ea812148a746b2daa64f0cd3df245c1
AndrewBloodworth <andrewcbld@gmail.com>
2022-05-06 13:20:29 -0600

    Added compression on Express config

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 //Allow Cross origin Cookies
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // body parsing middleware
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
